8d4363ad8f19840ef78ab58ed6e596f7cf06dbd4
supersmitty2018 <48794241+supersmitty2018@users.noreply.github.com>
2019-05-06 16:55:21 +0200

    Reset button added

--- script.js
+++ script.js
@@ -3,23 +3,40 @@ $(document).ready(function(){
    
     //Trigger event handler
     $("#optelBtn").click(function(){
-        var x = parseInt(document.getElementById("eerstewaarde").value);
-        var y = parseInt(document.getElementById("tweedewaarde").value);
+        var x = parseFloat(document.getElementById("eerstewaarde").value);
+        var y = parseFloat(document.getElementById("tweedewaarde").value);
         var result = x + y;
-        document.getElementById("result").innerHTML = "Het resultaat van de optelsom is: " + result;
-        reset(); 
+        document.getElementById("result").innerHTML = "Het resultaat van de som is: " + result;
+        reset();
+        document.getElementById("eerstewaarde").value = result; 
     });
 
     $("#aftrekBtn").click(function(){
-        var x = parseInt(document.getElementById("eerstewaarde").value);
-        var y = parseInt(document.getElementById("tweedewaarde").value);
+        var x = parseFloat(document.getElementById("eerstewaarde").value);
+        var y = parseFloat(document.getElementById("tweedewaarde").value);
         var result = x - y;
-        document.getElementById("result").innerHTML = "Het resultaat van de aftreksom is: " + result; 
+        document.getElementById("result").innerHTML = "Het resultaat van de som is: " + result; 
         reset();
+        document.getElementById("eerstewaarde").value = result; 
     });
+
+    $("#deelBtn").click(function(){
+        var x = parseFloat(document.getElementById("eerstewaarde").value);
+        var y = parseFloat(document.getElementById("tweedewaarde").value);
+        var result = x / y;
+        document.getElementById("result").innerHTML = "Het resultaat van de som is: " + result; 
+        reset();
+        document.getElementById("eerstewaarde").value = result; 
+    });
+
+    $("#resetBtn").click(function() {
+       reset();
+    });
+
+    function reset() {
+        document.getElementById("eerstewaarde").value = ""; 
+        document.getElementById("tweedewaarde").value = "";   
+    }
+
 });
     
-function reset() {
-    document.getElementById("eerstewaarde").value = ""; 
-    document.getElementById("tweedewaarde").value = ""; 
-}
